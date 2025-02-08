537fccea3475e37f79a0725d3f164eab3a6ca2c2
“Ashish-yadav2002” <ashishyadav5012002@gmail.com>
2025-02-09 02:17:48 +0530

    added contact

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { FaLinkedin,FaGithub,FaInstagram } from 'react-icons/fa'
 import styles from './Home.module.css'
 import Navbar from './Navbar/Navbar'
 import SkillsCaresoul from './SkillCaresoul/SkillsCaresoul'
 import ashish from '../assets/ashish.png'
 import Experience from './Experience/Experience'
+import { MdEmail } from 'react-icons/md'
 const Home = () => {
+  const [showButtons, setShowButtons] = useState(false)
   return (
     <div className={styles.home}>
       <div className={styles.Intro}>
@@ -12,7 +15,14 @@ const Home = () => {
           <h1>Hi, I'm </h1>
           <span id={styles.name}>Ashish Yadav.</span>
           <p id={styles.designation}>Full Stack Developer</p>
-          <button id={styles.contact}>Contact Me</button>
+          <button id={styles.contact} onClick={()=>setShowButtons(!showButtons)}>Contact Me
+            {showButtons && <div className={styles.contactButtons}>
+              <a href="https://www.linkedin.com/in/ashish-yadav-7b1b3b1b1/" target="_blank" rel="noreferrer"><FaLinkedin size="3vmax"/></a>
+              <a href="" tabIndex={0}><MdEmail size="3vmax" /></a>
+              <a href="" tabIndex={0}><FaGithub size="3vmax"/></a>
+              <a href="" tabIndex={0}><FaInstagram size="3vmax"/></a>
+            </div>}
+          </button>
         </div>
         <div className={styles.pannel2}>
           <img src={ashish} border="0" id={styles.myImage}></img>
